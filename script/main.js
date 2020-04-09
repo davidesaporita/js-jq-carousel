ab64377f2f8bc0581bbe9d844d23d72ee6184879
Davide Saporita <davide.saporita@gmail.com>
2020-04-09 17:31:19 +0200

    JS | Added keydown mode

--- script/main.js
+++ script/main.js
@@ -12,35 +12,51 @@ $( document ).ready(function() {
     
     var right = $('#right');
     var left = $('#left');
-    var gallery = $('#gallery');
+
+    right.click(goRight);
+    left.click(goLeft);
     
-    right.click(function() {
-        var activeImg = gallery.children('.active');
-
-        if(activeImg.next().attr('src') == undefined) {
-            var newImg = gallery.children('img').first();        
-        } else  {
-            var newImg = activeImg.next();
-        }      
-
-        activeImg.toggleClass('active');
-        newImg.toggleClass('active');
+    $(document).keydown(function(e) {
+        if(e.keyCode == 39) {
+            goRight();  
+        }
+        if(e.keyCode == 37) {
+            goLeft();
+        }
     });
-
-    left.click(function() {
-        var activeImg = gallery.children('.active');
-        console.log(activeImg.prev().attr('src'));
-
-        if(activeImg.prev().attr('src') == undefined) {
-            var newImg = gallery.children('img').last();        
-        } else  {
-            var newImg = activeImg.prev();
-        }      
-
-        activeImg.toggleClass('active');
-        newImg.toggleClass('active');
-    });
-    
 
 // End ready
 });
+
+
+// Right Click or Keydown function
+function goRight() {
+    var gallery = $('#gallery');
+    var activeImg = gallery.children('.active');
+
+    if(activeImg.next().attr('src') == undefined) {
+        var newImg = gallery.children('img').first();        
+    } else {
+        var newImg = activeImg.next();
+    }      
+
+    activeImg.toggleClass('active');
+    newImg.toggleClass('active');
+}
+
+
+
+// Left Click or Keydown function
+function goLeft() {
+    var gallery = $('#gallery');
+    var activeImg = gallery.children('.active');
+
+    if(activeImg.prev().attr('src') == undefined) {
+        var newImg = gallery.children('img').last();        
+    } else {
+        var newImg = activeImg.prev();
+    }
+
+    activeImg.toggleClass('active');
+    newImg.toggleClass('active');
+}
