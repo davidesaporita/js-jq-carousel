7c6fdd7cf6c7a91814d919d22dff59ec04375324
Davide Saporita <davide.saporita@gmail.com>
2020-04-09 15:23:32 +0200

    Added project files

---
+++ script/main.js
@@ -0,0 +1,10 @@
+/**
+ * Creare uno slider di immagini
+ * Potete usare le immagini che desiderate. Per facilitarvi la vita usate immagini delle stesse dimensioni
+ * Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+ * In oltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera
+ * Utilizziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider
+ * Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
+ *  
+ */
+
